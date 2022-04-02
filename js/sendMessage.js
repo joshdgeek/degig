b04531f504b078e23c7739ac7084cc6d83929f67
Osinachi <mhiztaaustin@gmail.com>
2022-04-02 23:39:19 +0100

    canceled parallel fetch request

--- js/sendMessage.js
+++ js/sendMessage.js
@@ -3,25 +3,25 @@ https://wa.me/number/?text=urlencodedtext
 
 */
 
-const form = document.querySelector("contact-form");
+const form = document.querySelector("#contact-form");
 const usernameField =  document.querySelector("#cf-name")
 const messageField = document.querySelector("#cf-message")
 const sendButton = document.querySelector("#cf-submit")
 
 
-sendButton.addEventListener("click",async ()=>{
+form.addEventListener("submit",async (e)=>{
+    e.preventDefault()
     let names = usernameField.value
     let message = messageField.value
 
    // alert(names)
-    const messageConstruct = `My name is ${name}.
+    const messageConstruct = `My name is ${names}.
      ${message}`
 
-    let url = `https://wa.me/234903741801/?text=${messageConstruct}`
+    let url = `https://wa.me/2348183585436/?text=${messageConstruct}`
     
     try {
-        //hit the whatsapp api endpoint
-        const res = await fetch(url)
+        location.assign(url)
     } catch (error) {
         res.json({error:error})
     }
