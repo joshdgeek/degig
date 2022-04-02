266245e5bdc88e113eb9b0f13801c7077f851202
Osinachi <mhiztaaustin@gmail.com>
2022-04-02 12:44:27 +0100

    created variables for form fields

--- js/sendMessage.js
+++ js/sendMessage.js
@@ -2,3 +2,14 @@
 https://wa.me/number/?text=urlencodedtext
 
 */
+
+const form = document.querySelector("contact-form");
+const name = form.cf-name.value
+const email = form.cf-email.value
+const subject = form.subject.value
+const message = form.cf-message.value  
+const sendButton = documnt.querySelector("#cf-submit")
+
+sendButton.addEventLisener("submit",(e)=>{
+    //run a parallel fetch requests
+})
