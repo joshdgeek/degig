3eb95cde9324cb18bbc473cc36eda57971299024
Osinachi <mhiztaaustin@gmail.com>
2022-04-02 13:52:38 +0100

    i wrote a script to run 4 parallel requests

--- js/sendMessage.js
+++ js/sendMessage.js
@@ -4,12 +4,29 @@ https://wa.me/number/?text=urlencodedtext
 */
 
 const form = document.querySelector("contact-form");
-const name = form.cf-name.value
-const email = form.cf-email.value
-const subject = form.subject.value
-const message = form.cf-message.value  
-const sendButton = documnt.querySelector("#cf-submit")
+const usernameField =  document.querySelector("#cf-name")
+const emailField = document.querySelector("#cf-email")
+const subjectField = document.querySelector("#cf-subject")
+const messageField = document.querySelector("#cf-message")
+const sendButton = document.querySelector("#cf-submit")
 
-sendButton.addEventLisener("submit",(e)=>{
+
+sendButton.addEventListener("click",async ()=>{
+    let name = usernameField.value
+    let email = emailField.value
+    let subject =  subjectField.value 
+    let message = messageField.value
     //run a parallel fetch requests
+
+    try {
+      //use promises to run parallel requests
+        const parallel = await Promise.all([
+            fetch("https://wa.me/number/?text=urlencodedtext"),
+            fetch("https://wa.me/number/?text=urlencodedtext"),
+            fetch("https://wa.me/number/?text=urlencodedtext"),
+            fetch("https://wa.me/number/?text=urlencodedtext")
+        ])
+    } catch (error) {
+        paralel.json({error:error})
+    }
 })
